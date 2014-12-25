f8e6780d21b69515e59c569bf97d00f8a9fe1629
Johnathan Pagnutti <Johnathan@Acrux.local>
2014-12-25 08:07:26 -0600

    Rewrote Knife Juggler to correctly work with upstream, wrote unit tests for juggles
    Also, wrote it such that juggles' throw is a random effect node that comes after a minion being placed (with each child being juggles hitting a different target),
    so that the AI can correctly look at all possible throw targets and see how they stack up (rather than how we had it, which was that juggles threw without generating any new nodes).
    
    In theory, this means we can log juggles' throw, but probably not in practice.

--- src/main/java/com/hearthsim/card/minion/concrete/KnifeJuggler.java
+++ src/main/java/com/hearthsim/card/minion/concrete/KnifeJuggler.java
@@ -1,38 +1,81 @@
 package com.hearthsim.card.minion.concrete;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hearthsim.card.minion.Hero;
 import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.Card;
+import com.hearthsim.card.CardPlayAfterInterface;
+import com.hearthsim.card.CardPlayBeginInterface;
 import com.hearthsim.card.Deck;
 import com.hearthsim.exception.HSException;
 import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.model.PlayerModel;
 import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.HearthAction;
 import com.hearthsim.util.tree.HearthTreeNode;
+import com.hearthsim.util.tree.RandomEffectNode;
 
-public class KnifeJuggler extends Minion {
+public class KnifeJuggler extends Minion implements CardPlayAfterInterface {
 	private final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
 	public KnifeJuggler() {
-		super();
-		
+		super();	
 	}
-	
+
 	@Override
-	public HearthTreeNode minionSummonEvent(PlayerSide thisMinionPlayerSide, PlayerSide summonedMinionPlayerSide,
-			Minion summonedMinion, HearthTreeNode boardState, Deck deckPlayer0, Deck deckPlayer1)
-			throws HSInvalidPlayerIndexException {
+	public HearthTreeNode onCardPlayResolve(PlayerSide thisCardPlayerSide,
+			PlayerSide cardUserPlayerSide, Card usedCard,
+			HearthTreeNode boardState, Deck deckPlayer0, Deck deckPlayer1,
+			boolean singleRealizationOnly) {
 		
 		HearthTreeNode toRet = boardState;
+		PlayerSide otherPlayerSide = cardUserPlayerSide.getOtherPlayer();
 		
-		if(thisMinionPlayerSide == summonedMinionPlayerSide) {
-			PlayerSide otherPlayerSide = thisMinionPlayerSide.getOtherPlayer();
-			int index = (int)(Math.random()*( otherPlayerSide.getPlayer(toRet).getNumCharacters() ));
-			Minion targetCharacter = toRet.data_.getCharacter(otherPlayerSide, index);
-			try {
-				toRet = targetCharacter.takeDamage((byte)1, otherPlayerSide.getOtherPlayer(), otherPlayerSide, toRet, deckPlayer0, deckPlayer1, false, false);
-			} catch (HSException e) {
-				log.error(e.getMessage());
-			}
+		if(usedCard == this){
+			return toRet;
 		}
-		return super.minionSummonEvent(thisMinionPlayerSide, summonedMinionPlayerSide, summonedMinion, toRet, deckPlayer0, deckPlayer1);
+		
+		if(singleRealizationOnly){
+			if(toRet != null){
+				int targetIndex = (int)(Math.random()*( otherPlayerSide.getPlayer(toRet).getNumCharacters() ));
+				Minion targetCharacter;
+				try {
+					targetCharacter = toRet.data_.getCharacter(otherPlayerSide, targetIndex);
+					targetCharacter.takeDamage((byte)1, otherPlayerSide.getOtherPlayer(), otherPlayerSide, toRet, deckPlayer0, deckPlayer1, false, false);
+				} catch (HSInvalidPlayerIndexException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (HSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}else{
+			List<Minion> possibleTargets = new ArrayList<Minion>();
+			possibleTargets = PlayerSide.WAITING_PLAYER.getPlayer(toRet).getMinions();
+			possibleTargets.add(PlayerSide.WAITING_PLAYER.getPlayer(toRet).getHero());
+			
+			if(possibleTargets.size() > 0){
+				toRet = new RandomEffectNode(toRet, new HearthAction(HearthAction.Verb.RNG)); //TODO: not proud of this implementation, and it might not work.  This needs to be an RNG action
+																							  //	  but I'm not entirely sure how to get the right params for the action
+				PlayerModel targetPlayer = PlayerSide.WAITING_PLAYER.getPlayer(toRet);
+				for(Minion possibleTarget : possibleTargets){
+					HearthTreeNode newState = new HearthTreeNode(toRet.data_.deepCopy());
+					Minion targetMinion = (possibleTarget instanceof Hero) ? PlayerSide.WAITING_PLAYER.getPlayer(newState).getHero() : PlayerSide.WAITING_PLAYER.getPlayer(newState).getMinions().get(targetPlayer.getMinions().indexOf(possibleTarget));
+					try {
+						targetMinion.takeDamage((byte)1, PlayerSide.CURRENT_PLAYER, PlayerSide.CURRENT_PLAYER, newState, deckPlayer0, deckPlayer1, false, false);
+					} catch (HSException e) {
+						e.printStackTrace();
+					}
+					toRet.addChild(newState);
+				}
+			}
+			
+		}
+		
+		return toRet;
 	}
 
 }
--- src/test/groovy/com/hearthsim/test/groovy/card/KnifeJugglerSpec.groovy
+++ src/test/groovy/com/hearthsim/test/groovy/card/KnifeJugglerSpec.groovy
@@ -15,28 +15,90 @@ import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
 import static org.junit.Assert.*
 
 
+/**
+ * Knife Juggler unit tests
+ * TODO: write this better-- it's not testing to make sure that cards are removed, or that the bloodfen raptor was
+ * correctly played, etc.
+ * 
+ * @author Johnathan
+ */
 public class KnifeJugglerSpec extends CardSpec {
 	HearthTreeNode root
 	BoardModel startingBoard
 
 	def setup() {
+		
+		def minionMana = 2;
+		def attack = 5;
+		def health0 = 3;
+		def health1 = 7;
+		
+		def commonField = [
+				[mana: minionMana, attack: attack, health: health0, maxHealth: health0], //todo: attack may be irrelevant here
+				[mana: minionMana, attack: attack, health: health1 - 1, maxHealth: health1]
+		]
+		
 
 		startingBoard = new BoardModelBuilder().make {
 			currentPlayer {
 				hand([KnifeJuggler, BloodfenRaptor])
 				mana(7)
-				deck([TheCoin, TheCoin])
 			}
 			waitingPlayer {
+				field(commonField)
 				mana(4)
-				deck([TheCoin, TheCoin])
 			}
 		}
 
 		root = new HearthTreeNode(startingBoard)
 	}
 	
-	def "cannot play for waiting player's side"() {
+	def "playing for current player returns expected child states"() {
+		def minionPlayedBoard = startingBoard.deepCopy()
+		def copiedRoot = new HearthTreeNode(minionPlayedBoard)
+		def knifeJugglerCard = minionPlayedBoard.getCurrentPlayerCardHand(0);
+		def ret = knifeJugglerCard.useOn(CURRENT_PLAYER, 0, copiedRoot, null, null);
+		
+		def knifeJugglerPlayed = ret.data_.deepCopy();
+		def nextCard = ret.data_.getCurrentPlayerCardHand(0);
+		def finalRet = nextCard.useOn(CURRENT_PLAYER, 0, ret, null, null);
+		
+		expect:
+		assertFalse(ret == null)
+		assertFalse(finalRet == null)
+		assertEquals(finalRet.numChildren(), 3)
+		
+		assertBoardDelta(startingBoard, knifeJugglerPlayed) {
+			currentPlayer {
+				playMinion(KnifeJuggler)
+				mana(5)
+			}
+		}
+	
+		HearthTreeNode child2 = finalRet.getChildren().get(2);
+		assertBoardDelta(finalRet.data_, child2.data_) {
+			waitingPlayer {
+				heroHealth(29)
+			}
+		}
+		
+		HearthTreeNode child0 = finalRet.getChildren().get(0);
+		assertBoardDelta(finalRet.data_, child0.data_){
+			waitingPlayer{
+				updateMinion(0, [deltaHealth: -1])
+			}
+		}
+		
+		HearthTreeNode child1 = finalRet.getChildren().get(1);
+		assertBoardDelta(finalRet.data_, child1.data_){
+			waitingPlayer{
+				updateMinion(1, [deltaHealth: -1])
+			}
+		}
+		
+	}
+/*
+		def "cannot play for waiting player's side"() {
 		def copiedBoard = startingBoard.deepCopy()
 		def target = root.data_.getCharacter(WAITING_PLAYER, 0)
 		def theCard = root.data_.getCurrentPlayerCardHand(0)
@@ -47,7 +109,7 @@ public class KnifeJugglerSpec extends CardSpec {
 		assertTrue(ret == null)
 		assertEquals(copiedBoard, startingBoard)
 	}
-/*
+
 	def "playing KnifeJuggler"() {
 		def cards = [ new TheCoin(), new TheCoin() ]
 		def deck = new Deck(cards)
